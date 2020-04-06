3da6d877ed301e0d303e12dba4c348000e326613
Glitch (bot-oscuro-5894423) <none>
2020-04-06 15:13:02 +0000

    🏹🍡 Updated with Glitch

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
 require("./lib/router/");
 require("./lib/bot/");
-const a = require("./lib/functions/profil");
-function callback(texte){
-  console.log(texte)
-}
-a.voir_profil("675632122765115448",callback);
-a.supprimer_profil("675632122765115448");
